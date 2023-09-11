d486bcf1315f63437ac8b072eb77be0d68c19fc2
Ananiya <anewscho@gmail.com>
2023-09-11 16:35:51 +0300

    max char done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) 
+{
+    let dict={};
+    let max=0;
+    let maxchar='';
+    for(let char of str)
+    {
+        dict[char]=dict[char]+1 || 1
+
+    }
+   for(let char in dict)
+   {
+    if(dict[char]>max)
+    {
+        max=dict[char];
+        maxchar=char;
+    }
+   }
+   return maxchar
+}
 
 module.exports = maxChar;
