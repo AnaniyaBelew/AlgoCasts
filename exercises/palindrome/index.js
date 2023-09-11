2306a79f37c874952ff07c521345ce3fc95c62e2
Ananiya <anewscho@gmail.com>
2023-09-11 15:30:31 +0300

    pallindromes done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) 
+{
+    return str.split('').every((char,i)=>
+    {
+        return char===str[str.length-i-1];
+    })
+    
+}
 
 module.exports = palindrome;
+
+// check= str.split('').reduce((rev,c)=>c+rev,'')
+//     return str===check
