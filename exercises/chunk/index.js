ae3c39f2ab843c04fce54b15d0159317a5765fb6
Ananiya <anewscho@gmail.com>
2023-09-11 17:16:32 +0300

    chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,20 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) 
+{
+    let chunked=[];
+    for(let x of array)
+    {
+        const last=chunked[chunked.length-1];
+        if(!last || last.length===size)
+        {
+            chunked.push([x])
+        }
+        else last.push(x)
+    }
+return chunked
+   
+}
 
 module.exports = chunk;
