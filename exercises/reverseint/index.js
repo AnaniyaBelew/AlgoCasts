c00dee00e57f295ca77ce768a1b09e705789b32d
Ananiya <anewscho@gmail.com>
2023-09-11 16:06:32 +0300

    reverseint done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) 
+{
+    const number=parseInt(n.toString().split('').reverse().join(''))
+    return number* Math.sign(n)
+  
+}
 
 module.exports = reverseInt;
+
+// const sign=Math.sign(n)
+// if(sign===1)return parseInt(n.toString().split('').reverse().join(''))
+// if(sign===-1) return -parseInt(n.toString().split('').reverse().join(''))
+// else return 0
